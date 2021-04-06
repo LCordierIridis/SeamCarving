15f6661e7c0d392eeaecadabf7672f5371f461c6
Lucas Cordier <lucas.cordier@edu.esiee.fr>
2021-04-06 14:06:50 +0200

    First commit

---
+++ SeamCarving.py
@@ -0,0 +1,5 @@
+# TODO : Energy function
+# Dual gradient energy function : https://www.cs.princeton.edu/courses/archive/fall13/cos226/assignments/seamCarving.html
+
+# Calculate difference between left and right pixels for each color
+# Calculate difference between up and down pixels for each color
